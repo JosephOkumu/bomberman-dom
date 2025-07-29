7c4f0a113033acaa195f2dc236decaca6ee47671
JosephOkumu <jokumu25@gmail.com>
2025-07-29 20:22:25 +0300

    feat: initialize game board with four players

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ const initialState = {
   user: null,
   data: [],
   counter: 0,
-  nickname: null
+  nickname: null,
+  game: {
+    board: null,
+    players: [
+      { id: 1, x: 1, y: 1, direction: 'down', lives: 3, active: true },
+      { id: 2, x: 29, y: 1, direction: 'down', lives: 3, active: false },
+      { id: 3, x: 1, y: 11, direction: 'down', lives: 3, active: false },
+      { id: 4, x: 29, y: 11, direction: 'down', lives: 3, active: false }
+    ],
+    currentPlayer: 1
+  }
 };
 
 function update(state, msg) {
@@ -19,7 +29,7 @@ function update(state, msg) {
     case "DECREMENT":
       return { ...state, counter: state.counter - 1 };
     case "ROUTE_CHANGE":
-      return { 
+      const newState = { 
         ...state, 
         path: msg.path,
         nickname: msg.nickname || state.nickname
