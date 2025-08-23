560a852d1dd448f17e4dda9fa77fe794958265ec
jkuya <kuyajesee@proton.me>
2025-08-23 18:51:44 +0300

    feat: enhance waiting room display with dynamic player badges and avatar paths

--- pages/waiting.js
+++ pages/waiting.js
@@ -1,14 +1,38 @@
 import domParser from "../MiniMvc/domParser.js";
 
 export default (state) => {
+  const { waitingPlayers, currentPlayerId } = state;
+
+  const getAvatarPath = (avatar) => {
+    switch (avatar) {
+      case 'B1':
+        return 'assets/B1/b1walkDown.png';
+      case 'B2':
+        return 'assets/B2/walkingdown.png';
+      case 'B3':
+        return 'assets/B3/down.png';
+      case 'B4':
+        return 'assets/B4/down.png';
+      default:
+        return '';
+    }
+  };
+
+  const playerBadgesHTML = waitingPlayers.map(player => `
+    <div class="player-badge ${player.id === currentPlayerId ? 'current-player' : ''}">
+      <div class="player-avatar" style="background-image: url('${getAvatarPath(player.avatar)}'); background-size: 300% 100%; background-repeat: no-repeat; background-position: 0 0;"></div>
+      <p class="player-name">${player.nickname}</p>
+    </div>
+  `).join('');
+
   const htmlString = `
     <section id="waiting-room-screen" class="screen">
         <div class="card">
             <h1>Waiting for Players</h1>
             <div id="player-badges-container" class="player-badges">
-                <!-- Player badges will be dynamically inserted here -->
+                ${playerBadgesHTML}
             </div>
-            <p id="player-count" class="player-count">1/4 joined</p>
+            <p id="player-count" class="player-count">${waitingPlayers.length}/4 joined</p>
             <div class="timers">
                 <div class="timer-display">
                     <p>Auto-start with 2+ players in:</p>
